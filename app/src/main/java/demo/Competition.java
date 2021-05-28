eadf0ffca91ec37be91594fd6119ee6b9a1c7342
D0543150 <yhc78887242@gmail.com>
2021-05-28 17:54:19 +0800

    First test

---
+++ app/src/main/java/demo/Competition.java
@@ -0,0 +1,58 @@
+package competition;
+
+
+public class Competition {
+	
+	
+	private int coach;
+	private int student;
+	private double s_time;
+	private double e_time;
+	private String w_time;
+	private int all;
+	
+	public Competition(int coach,int student,double s_time,double e_time,String w_time) {
+		this.coach = coach;
+		this.student = student;
+		this.s_time = s_time;
+		this.e_time = e_time;
+		this.w_time = w_time;
+		all = coach + student;
+	}
+	
+	public String rent(){
+		int money;
+		
+		if(coach<1) {
+			return "project:normal";
+		}
+		else if(all < 10) {
+			if(s_time < 8 || (s_time >= 10 && s_time < 10.3) || (s_time >= 12.3 && s_time < 14) || (s_time >= 16 && s_time < 16.3) || (s_time >= 18.3 && s_time < 19) || s_time >= 21 || e_time < 8 || (e_time > 10 && e_time < 10.3) || (e_time > 12.3 && e_time < 14) || (e_time > 16 && e_time < 16.3) || (e_time > 18.3 && e_time < 19) || e_time > 21)
+			{
+				return "wrong time";
+			}
+			else if(s_time + 1 != e_time && s_time + 2 != e_time) {
+				return "plz rent 1 or 2 hours";
+			}
+			else {
+				if(e_time-s_time == 2) {
+					return "project:one lane $6000";
+				}
+				else{
+					return "project:one lane $3000";
+				}
+			}	
+		}
+		else {
+			if(w_time != "A" && w_time != "B" && w_time != "C" && w_time != "D" && w_time != "E") {
+				return "wrong time label";
+			}
+			else {
+				money = coach * 120 + student * 70;
+				
+				return "project:time rent $" + Integer.toString(money);
+			}
+		}
+		
+	}
+}
---
+++ app/src/test/java/demo/CompetitionTest.java
@@ -0,0 +1,141 @@
+package competition;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CompetitionTest {
+
+	@Test
+	void test() {
+		Competition a = new Competition(0,10,8,1,"A");
+		assertEquals("project:normal",a.rent());
+	}
+	
+	@Test
+	void test1() {
+		Competition a = new Competition(1,3,7,1,"A");
+		assertEquals("wrong time",a.rent());
+	}
+	
+	@Test
+	void test2() {
+		Competition a = new Competition(1,3,10,1,"A");
+		assertEquals("wrong time",a.rent());
+	}
+
+	@Test
+	void test3() {
+		Competition a = new Competition(1,3,13,1,"A");
+		assertEquals("wrong time",a.rent());
+	}
+	
+	@Test
+	void test4() {
+		Competition a = new Competition(1,3,16,1,"A");
+		assertEquals("wrong time",a.rent());
+	}
+	
+	@Test
+	void test5() {
+		Competition a = new Competition(1,3,18.3,1,"A");
+		assertEquals("wrong time",a.rent());
+	}
+	
+	@Test
+	void test6() {
+		Competition a = new Competition(1,3,21,1,"A");
+		assertEquals("wrong time",a.rent());
+	}
+	
+	@Test
+	void test7() {
+		Competition a = new Competition(1,3,8,7,"A");
+		assertEquals("wrong time",a.rent());
+	}
+	
+	@Test
+	void test8() {
+		Competition a = new Competition(1,3,8,10.2,"A");
+		assertEquals("wrong time",a.rent());
+	}
+	
+	@Test
+	void test9() {
+		Competition a = new Competition(1,3,8,13,"A");
+		assertEquals("wrong time",a.rent());
+	}
+	
+	@Test
+	void test10() {
+		Competition a = new Competition(1,3,8,16.2,"A");
+		assertEquals("wrong time",a.rent());
+	}
+	
+	@Test
+	void test11() {
+		Competition a = new Competition(1,3,8,18.4,"A");
+		assertEquals("wrong time",a.rent());
+	}
+	
+	@Test
+	void test12() {
+		Competition a = new Competition(1,3,8,22,"A");
+		assertEquals("wrong time",a.rent());
+	}
+	/*
+	@Test
+	void test13() {
+		Competition a = new Competition(1,3,8,9.3,"A");
+		assertEquals("plz rent 1 or 2 hours",a.rent());
+	}
+	
+	@Test
+	void test14() {
+		Competition a = new Competition(1,3,8,10,"A");
+		assertEquals("project:one lane $6000",a.rent());
+	}
+	
+	@Test
+	void test15() {
+		Competition a = new Competition(1,3,8,9,"A");
+		assertEquals("project:one lane $3000",a.rent());
+	}
+	
+	@Test
+	void test16() {
+		Competition a = new Competition(1,9,8,10,"G");
+		assertEquals("wrong time label",a.rent());
+	}
+	
+	@Test
+	void test17() {
+		Competition a = new Competition(1,9,8,10,"A");
+		assertEquals("project:time rent $750",a.rent());
+	}
+	
+	@Test
+	void test18() {
+		Competition a = new Competition(1,9,8,10,"B");
+		assertEquals("project:time rent $750",a.rent());
+	}
+	
+	@Test
+	void test19() {
+		Competition a = new Competition(1,9,8,10,"C");
+		assertEquals("project:time rent $750",a.rent());
+	}
+	
+	@Test
+	void test20() {
+		Competition a = new Competition(1,9,8,10,"D");
+		assertEquals("project:time rent $750",a.rent());
+	}
+	
+	@Test
+	void test21() {
+		Competition a = new Competition(1,9,8,10,"E");
+		assertEquals("project:time rent $750",a.rent());
+	}
+	*/
+}
